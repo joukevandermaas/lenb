08604274207abe32ff62c4232a80e812939e9bee
Jouke van der Maas <jouke.vandermaas@gmail.com>
2013-01-09 00:02:25 +0100

    Header files voor de nieuwe data toegevoegd

--- generate_arff.py
+++ generate_arff.py
@@ -1,3 +1,8 @@
+# Use: python generate_arff.py <filename.csv>
+# Output: a file called filename.arff in the same folder.
+# This script requires a file called arff_attributes/filename.txt in
+# the same folder as the original file.
+
 import sys
 import os.path
 
@@ -9,10 +14,13 @@ orig = open(filename, "rt")
 arff = open(dir + name + ".arff", "wt")
 attr = open(dir + "/arff_attributes/" + name + ".txt", "rt")
 
+# write arff header
 arff.write("@relation " + name + "\n\n")
 arff.write(attr.read())
+
+# just dump data (it's csv anyway)
 arff.write("\n\n@data\n")
-arff.writelines(orig.readlines()[1:-1])
+arff.writelines(orig.readlines())
 
 orig.close()
 arff.close()
