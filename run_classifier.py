35dc7655ebf94392a50db9d3797f6dee1580be2e
Jouke van der Maas <jouke.vandermaas@gmail.com>
2013-01-10 14:21:08 +0100

    Script genereert resultaat voor met/zonder gps-data

--- run_classifier.py
+++ run_classifier.py
@@ -4,5 +4,7 @@ import subprocess
 from weka_functions import *
 
 filename = "data/appended/appended_split_gps_predictors_behaviour"
-remove_columns("24,26", filename + ".arff", filename + ".tmp.arff")
-apply_classifier("trees.J48", filename + ".tmp.arff", filename + ".arff.txt")
+remove_columns("3-6,25,26", filename + ".arff", "1.tmp.arff")
+apply_classifier("trees.J48", "1.tmp.arff", "nogps.arff.txt")
+remove_columns("25,26", filename + ".arff", "2.tmp.arff")
+apply_classifier("trees.J48", "2.tmp.arff", "gps.arff.txt")
--- weka_functions.py
+++ weka_functions.py
@@ -8,10 +8,13 @@ def remove_columns(columns, input, output):
               "-o", add_quotes(output)])  # output file
     
 def apply_classifier(classifier, input, output):
+    file = open(output, "wt")
     subprocess.Popen(["java", "weka.classifiers." + classifier, 
                       "-t", add_quotes(input),  # input file
-                      "-x", "4"],               # 4 folds
-                     stdout=open(output, "wt")) # write stdout to file
+                      "-C", "0.05", # confidence threshold
+                      "-x", "4", "-i"],         # 4 folds, output more info
+                     stdout=file) # write stdout to file
+    file.close()
 
 def add_quotes(string):
     return string#"\"" + string + "\""
