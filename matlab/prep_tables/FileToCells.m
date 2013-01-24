6d32b0ae9bb9227ca19eaca3a9a490deb3cee352
Jouke van der Maas <jouke.vandermaas@gmail.com>
2013-01-24 17:40:32 +0100

    file-to-cells

--- matlab/prep_tables/FileToCells.m
+++ matlab/prep_tables/FileToCells.m
@@ -1,6 +1,7 @@
-function Data = FileToCells(filename, delimiter)
+function [Data Header] = FileToCells(filename, delimiter)
 %   Reads data from file and removes the first row
     Data = read_mixed_csv(filename, delimiter);
     % Remove labels from data
+    Header = Data(1,:);
     Data(1,:) = [];
 end
