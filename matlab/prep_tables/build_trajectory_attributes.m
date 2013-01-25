0fc19a26a4b95d32f2ba66bbcea3e0fb4f4b2f9c
Koen Keune <koenkeune@live.nl>
2013-01-25 15:22:41 +0100

    turtuosity ipv acceleration

--- matlab/prep_tables/build_trajectory_attributes.m
+++ matlab/prep_tables/build_trajectory_attributes.m
@@ -1,12 +1,12 @@
 function [attributes, header] = build_trajectory_attributes(data, timestamps, indices)
     header = {'obsID', 'birdID', 'day', 'min', 'speed', ...
-       'acceleration', 'area', 'time', 'ab_dist', 'tot_dist'};
+       'tortuosity', 'area', 'time', 'ab_dist', 'tot_dist'};
 
     attributes = cell(length(indices)-1, length(header));
     chunk_times = timestamps(indices);
     text_speeds = data(indices, 8);
-    num_speeds = clean_speeds(text_speeds);
-    acceleration = diff(num_speeds)./diff(chunk_times);
+    %num_speeds = clean_speeds(text_speeds);
+    %acceleration = diff(num_speeds)./diff(chunk_times); % dy/dx
     %acceleration = gfilter(num_speeds, 2, [0 1])
 
     chunk = data(indices,:);
@@ -19,12 +19,14 @@ function [attributes, header] = build_trajectory_attributes(data, timestamps, in
 
     totalDistance = sum(diag(distances, 1));
     directDistance = pdist(numericData([1,end], [3 4]));
+    tortuosity = totalDistance / directDistance;
 
     totalTime = pdist(chunk_times([1 end])) * 100;
 
+    %num2str(acceleration(j-1), '%f'), ...
     for j = 2:length(indices)
         point = [data(indices(j), [1 2 3 4 8]), ...
-            num2str(acceleration(j-1), '%f'), ...
+            num2str(tortuosity, '%f'), ...
             num2str(totalArea, '%f'), ...
             num2str(totalTime, '%f'), ...
             num2str(directDistance, '%f'), ...
